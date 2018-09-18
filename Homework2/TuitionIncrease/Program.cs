f8e7b7babe549da22cae61e14d028105b3b562a7
SOONER\acke9387 <adam@ou.edu>
2018-09-18 14:28:46 -0500

    Initial Commit

---
+++ Homework2/TuitionIncrease/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuitionIncrease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
